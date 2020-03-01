4a1b5b9c7e2a8adaefc1fd7a4c3f47714ccd8681
yevgenyzubrin <yevgeniy.zubrin@gmail.com>
2020-03-01 22:53:50 +0600

    started hostels page

--- js/modal_hotel_search.js
+++ js/modal_hotel_search.js
@@ -6,4 +6,14 @@ motelSearchOpen.addEventListener('click', function(evt){
     evt.preventDefault();
     motelSearch.classList.toggle("modal-show");
     firstField.focus();
+});
+
+window.addEventListener('keyup', function (evt) {
+    if(evt.keyCode === 27) {
+        evt.preventDefault();
+
+        if(motelSearch.classList.contains("modal-show")) {
+                    motelSearch.classList.remove("modal-show");
+                }
+    }
 })
