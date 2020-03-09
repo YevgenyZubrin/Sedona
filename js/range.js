84ad7a7d8549f180e77dc98908bf8a726e068d25
yevgenyzubrin <yevgeniy.zubrin@gmail.com>
2020-03-09 15:54:22 +0600

    fixed range input

--- js/range.js
+++ js/range.js
@@ -4,8 +4,12 @@ var rangeValueText = document.querySelector(".filters-field__text");
 rangeValueText.value = rangeValue.value;
 
 rangeValue.addEventListener('change', function(evt) {
-    evt.preventDefault();
     rangeValueText.value = rangeValue.value;
+});
+
+rangeValue.addEventListener('mousemove', function(evt) {
+    rangeValueText.value = rangeValue.value;
+    
 });
 
 rangeValueText.addEventListener('change', function(evt) {
